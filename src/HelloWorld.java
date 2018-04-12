a6546882c2ee184bccc08a6802b9f550e8bdf349
narendra1989 <narendrarajput3004@gmail.com>
2018-04-12 15:30:16 +0530

    hello java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,4 @@
 /**
- * 
- */
-
-/**
  * @author narendra.rajput
  *
  */
@@ -12,6 +8,8 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("Hello Java program");
 	}
+	
 
 }
